07b86b9d066c99a896b18e27ce899a9b4b1645b3
Scaffolder <scaffolder@backstage.io>
2025-02-28 09:47:17 +0000

    initial commit

---
+++ main.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_providers {
+    confluent = {
+      source  = "confluentinc/confluent"
+      version = ">= 0.2.0"  # Adjust this version constraint as needed.
+    }
+  }
+}
+provider "confluent" {
+  # Use your Confluent Cloud credentials (set these via environment variables)
+  # For example, export CONFLUENT_CLOUD_API_KEY and CONFLUENT_CLOUD_API_SECRET
+}
+
+variable "environment_name" {
+  type    = string
+  default = "hdsakhdfsakjhadd"
+}
+
+
+resource "confluent_environment" "this" {
+  display_name = var.environment_name
+}
+
+# Add outputs to be used in documentation
+output "environment_id" {
+  value = confluent_environment.this.id
+  description = "The ID of the created Confluent Cloud environment"
+}
+
+output "environment_name" {
+  value = confluent_environment.this.display_name
+  description = "The name of the created Confluent Cloud environment"
+}
